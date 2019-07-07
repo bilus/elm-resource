2a13f461893c7a9d4d9e28c8456ad2cb2793fa16
Marcin Bilski <gyamtso@gmail.com>
2019-07-07 21:26:33 +0200

    Columns for each resource with sticky headers.

---
+++ Main.elm
@@ -0,0 +1,152 @@
+module Main exposing (main)
+
+import Browser
+import Duration as Duration exposing (Duration)
+import Element exposing (..)
+import Element.Background as Background
+import Html exposing (Html)
+import Html.Attributes exposing (style)
+import Time exposing (Posix)
+
+
+type Page
+    = InputPage
+
+
+type Msg
+    = NoOp
+
+
+type alias Model =
+    { currPage : Page, timetable : List Schedule }
+
+
+type alias Flags =
+    ()
+
+
+type Resource
+    = Resource { id : ResourceId, name : String }
+
+
+newResource : ResourceId -> String -> Resource
+newResource id name =
+    Resource { id = id, name = name }
+
+
+type ResourceId
+    = ResourceId String
+
+
+type Reservation
+    = Reservation { id : ReservationId, start : Posix, duration : Duration }
+
+
+type ReservationId
+    = ReservationId String
+
+
+type Schedule
+    = Schedule Resource (List Reservation)
+
+
+sampleTimetable =
+    [ Schedule
+        (newResource (ResourceId "id1") "ZS 672AE")
+        []
+    , Schedule
+        (newResource (ResourceId "id1") "ZS 8127S")
+        []
+    ]
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { currPage = InputPage, timetable = sampleTimetable }
+    , Cmd.none
+    )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        _ ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "VisExp"
+    , body =
+        [ layout [] <| viewTimetable model.timetable
+        ]
+    }
+
+
+viewTimetable : List Schedule -> Element Msg
+viewTimetable timetable =
+    row [ height fill ]
+        [ column [ width fill ]
+            [ row [ height fill ]
+                [ text "Hello world" ]
+            , row
+                [ height fill, width fill ]
+              <|
+                List.map viewSchedule timetable
+            ]
+        ]
+
+
+sticky : List (Attribute msg)
+sticky =
+    List.map htmlAttribute [ style "position" "sticky", style "top" "0" ]
+
+
+stickyHeader : String -> Attribute Msg
+stickyHeader title =
+    let
+        header =
+            row
+                ([ width fill
+                 , Background.color <| rgb255 200 200 200
+                 , padding 3
+                 ]
+                    ++ sticky
+                )
+                [ text title
+                ]
+    in
+    inFront <| header
+
+
+viewSchedule : Schedule -> Element Msg
+viewSchedule (Schedule (Resource { name }) _) =
+    column [ height fill, width fill, stickyHeader name, paddingXY 0 30 ] <|
+        [ paragraph [] [ text "Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.\n\nNullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.\n\n", text "Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.\n\nNullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.\n\n", text "Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.\n\nNullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.\n\n" ] ]
+
+
+main : Program Flags Model Msg
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
